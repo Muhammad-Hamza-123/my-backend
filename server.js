5cfd6519a26b174986cf0b08295650f201aa5448
Your Name <you@example.com>
2025-05-28 23:33:27 +0500

    Add proper CORS config for Vercel frontend

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 // Security Middleware
 app.use(helmet());
 const allowedOrigins = [
-  'https://your-vercel-project.vercel.app',
+  'https://my-frontend-xi.vercel.app/',
   'http://localhost:3000', // optional, for dev
 ];
 
